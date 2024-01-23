66263863267b6d7d6ba4a82d9b2711e0ed17a9f9
Nazar Taran <nazar.taran.id@gmail.com>
2024-01-23 22:34:31 +0100

    Add building dependencies and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import typescript from '@rollup/plugin-typescript';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [solid()],
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "dist",
+        }),
+      ],
+    },
+  },
 })
